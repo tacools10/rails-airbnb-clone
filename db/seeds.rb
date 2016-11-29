79f17f09953aa3ebd38042a98f3a0eb2e13d0178
Thomas Cools <tacools10@gmail.com>
2016-11-29 17:11:20 +0100

    Progress on login CSS and FB authorization

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,7 @@ User.destroy_all
 
 
 
-User.create(email: "kostas1@gmail.com", password:"123456", name: "Kostas", last_name: "Sofianopoulos", birthdate: Date.new(2001,2,3))
+User.create(email: "kostas1@gmail.com", password:"123456", first_name: "Kostas", last_name: "Sofianopoulos", birthdate: Date.new(2001,2,3))
 Asset.create(title: "Lucas' house", description: "In the jungle", price: "125000", address: "453 Amazon way", country: "Brasil", city: "Parana", region: "Don't know", condition: "good", year_built: 1990, year_reno: 1995, bedrooms: 2, bathrooms: 2, garage: 1, lots_size: 300, previous_owners: "Lucas' mom and dad", status: "Junk", user_id: User.first.id)
 Asset.create(title: "Lucas' house", description: "In the jungle", price: "125000", address: "452 Amazon way", country: "Brasil", city: "Parana", region: "Don't know", condition: "good", year_built: 1990, year_reno: 1995, bedrooms: 2, bathrooms: 2, garage: 1, lots_size: 300, previous_owners: "Lucas' mom and dad", status: "Junk", user_id: User.first.id)
 
