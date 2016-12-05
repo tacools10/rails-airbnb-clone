c7a0539c65f6aef11ad8608638da65bbee48f157
Thomas Cools <tacools10@gmail.com>
2016-12-05 15:23:26 +0100

    Added search functionality using solr

--- app/controllers/admin/assets_controller.rb
+++ app/controllers/admin/assets_controller.rb
@@ -3,16 +3,20 @@ class Admin::AssetsController < ApplicationController
   def index
     #@assets = Asset.all
     if params[:query]
-      @assets = Asset.where(city: params[:query])
+      @assets_search = Asset.search do
+        without :latitude, nil
+        without :longitude, nil
+        keywords(params[:query])
+        paginate(:page => (params[:page] or 1), :per_page => 30)
+      end
+      @assets = @assets_search.results
     else
       @assets = Asset.all
     end
-    @all_assets_hash = Gmaps4rails.build_markers(@assets.where.not(latitude: nil)) do |asset, marker|
+    @all_assets_hash = Gmaps4rails.build_markers(@assets.class.name == "ActiveRecord::Relation" ? @assets.where.not(latitude: nil) : @assets) do |asset, marker|
         marker.lat asset.latitude
         marker.lng asset.longitude
-      end
-
+    end
   end
-
 end
 
