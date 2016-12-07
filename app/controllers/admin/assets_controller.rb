e134d268ad5b374d1f61da0cca1a2db83ae04486
Thomas Cools <tacools10@gmail.com>
2016-12-07 15:12:50 +0100

    Added functioning filters

--- app/controllers/admin/assets_controller.rb
+++ app/controllers/admin/assets_controller.rb
@@ -4,21 +4,32 @@ class Admin::AssetsController < ApplicationController
     @price_ranges = [{to: 200000}, {from: 200000, to: 500000}, {from: 500000}]
   end
 
+  def search(params)
+    query = params[:query].presence || '*'
+      conditions = {}
+      conditions[:price] = (if params[:query_price_min].present? && params[:query_price_max].blank?
+                              {gte: params[:query_price_min]}
+                           elsif params[:query_price_max].present? && params[:query_price_min].blank?
+                              {lte: params[:query_price_max]}
+                           else params[:query_price_min].present? && params[:query_price_max].present?
+                              {gte: params[:query_price_min], lte: params[:query_price_max]}
+                           end) if params[:query_price_min].present? || params[:query_price_max].present?
+      conditions[:city] = Asset.find(params[:city]).city if params[:city].present?
+      conditions[:country] = Asset.find(params[:country]).country if params[:country].present?
+      @assets_search = Asset.search query, where: conditions
+      @assets = @assets_search.results
+    end
+
 
   def index
-    #@assets = Asset.all
-    price_ranges = self.ranges
     @disable_footer = true
-    if params[:query]
-      @assets_search = Asset.search params[:query], fields: [:title, :city], where: {latitude: {not: nil}}
-      @assets = @assets_search.results
 
-    # Add min / max plus add in filters for latitude / longitude. Should be able to repeat this process for other features like
-    # size, etc. in order to create variety of filters. Can even create dropdown form now that I understand
-    # the process.
-    elsif params[:query_price_min] || params[:query_price_max]
-      @assets_search = Asset.search '*', where: {price: {gte: params[:query_price_min], lte: params[:query_price_max]}}
-      @assets = @assets_search.results
+    if params[:query_price_min] || params[:query_price_max] || params[:city]
+      @assets = search(params)
+      respond_to do |format|
+        format.html {redirect_to admin_assets_path}
+        format.js
+      end
     else
       @assets = Asset.all
     end
