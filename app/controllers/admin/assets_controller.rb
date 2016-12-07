d1d85b9a5d73da50186ff1f4a74563d14728600f
Thomas Cools <tacools10@gmail.com>
2016-12-07 10:42:44 +0100

    Updated search more

--- app/controllers/admin/assets_controller.rb
+++ app/controllers/admin/assets_controller.rb
@@ -1,9 +1,23 @@
 class Admin::AssetsController < ApplicationController
 
+  def ranges
+    @price_ranges = [{to: 200000}, {from: 200000, to: 500000}, {from: 500000}]
+  end
+
+
   def index
     #@assets = Asset.all
+    price_ranges = self.ranges
+    @disable_footer = true
     if params[:query]
       @assets_search = Asset.search params[:query], fields: [:title, :city], where: {latitude: {not: nil}}
+      @assets = @assets_search.results
+
+    # Add min / max plus add in filters for latitude / longitude. Should be able to repeat this process for other features like
+    # size, etc. in order to create variety of filters. Can even create dropdown form now that I understand
+    # the process.
+    elsif params[:query_price_min] || params[:query_price_max]
+      @assets_search = Asset.search '*', where: {price: {gte: params[:query_price_min], lte: params[:query_price_max]}}
       @assets = @assets_search.results
     else
       @assets = Asset.all
