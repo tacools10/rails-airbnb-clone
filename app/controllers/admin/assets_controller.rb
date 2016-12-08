939e4a6298adc86f8c9d0f2915ac702104c9d402
Thomas Cools <tacools10@gmail.com>
2016-12-08 11:54:11 +0100

    Fixed multiple google maps API calls

--- app/controllers/admin/assets_controller.rb
+++ app/controllers/admin/assets_controller.rb
@@ -4,6 +4,9 @@ class Admin::AssetsController < ApplicationController
     @price_ranges = [{to: 200000}, {from: 200000, to: 500000}, {from: 500000}]
   end
 
+
+
+
   def search(params)
     query = params[:query].presence || '*'
       fields = [:address, :city, :post_code]
@@ -17,7 +20,7 @@ class Admin::AssetsController < ApplicationController
                            end) if params[:query_price_min].present? || params[:query_price_max].present?
       conditions[:city] = Asset.find(params[:city]).city if params[:city].present?
       conditions[:country] = Asset.find(params[:country]).country if params[:country].present?
-      conditions[:location] = Geocoder.coordinates(params[:asset[address]])
+      conditions[:location] = Geocoder.coordinates(params[:asset["address"]]) if params[:asset["address"]].present?
       @assets_search = Asset.search query, fields: fields, where: conditions
       @assets = @assets_search.results
   end
@@ -29,11 +32,6 @@ class Admin::AssetsController < ApplicationController
     if params[:query_price_min] || params[:query_price_max] || params[:city]
       @assets = search(params)
       respond_to do |format|
-        @all_assets_hash = Gmaps4rails.build_markers(@assets.class.name == "ActiveRecord::Relation" ? @assets.where.not(latitude: nil) : @assets) do |asset, marker|
-          marker.lat asset.latitude
-          marker.lng asset.longitude
-          marker.infowindow render_to_string(partial: "/assets/map_box", locals: { asset: asset })
-          end
         format.html {render 'assets/assets'}
         format.js
       end
@@ -41,21 +39,14 @@ class Admin::AssetsController < ApplicationController
       # Need to figure out how to keep dropdown form available / refresh
     elsif params[:query]
       @assets = search(params)
-      @all_assets_hash = Gmaps4rails.build_markers(@assets.class.name == "ActiveRecord::Relation" ? @assets.where.not(latitude: nil) : @assets) do |asset, marker|
-          marker.lat asset.latitude
-          marker.lng asset.longitude
-          marker.infowindow render_to_string(partial: "/assets/map_box", locals: { asset: asset })
-      end
     else
       @assets = Asset.all
+    end
       @all_assets_hash = Gmaps4rails.build_markers(@assets.class.name == "ActiveRecord::Relation" ? @assets.where.not(latitude: nil) : @assets) do |asset, marker|
         marker.lat asset.latitude
         marker.lng asset.longitude
         marker.infowindow render_to_string(partial: "/assets/map_box", locals: { asset: asset })
-     end
+      end
     end
-
-  end
-
 end
 
