cf6d1cb521af45705fbed86f242d816ccf001512
lucas souza <lfcsvt@gmail.com>
2016-12-12 19:29:22 +0100

    new

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,6 +42,9 @@ Rails.application.configure do
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
+  #mailer
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: "yourapp.herokuapp.com" }
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
   config.log_level = :debug
