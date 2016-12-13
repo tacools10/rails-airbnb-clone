a9e374af513a5b48610049032153996ad67cc675
Thomas Cools <tacools10@gmail.com>
2016-12-13 18:26:59 +0100

    Created custom map icons, infowindows, and tables

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,6 +1,13 @@
 var map;
 var markersArray = [];
 var bounds = new google.maps.LatLngBounds();
+var image = {
+  url: 'http://res.cloudinary.com/djlrrh291/image/upload/v1481648518/mapsicon_hovifq.png',
+  scaledSize: new google.maps.Size(20,20),
+  origin: new google.maps.Point(0,0),
+  anchor: new google.maps.Point(0,20)
+};
+var assets = gon.assets;
 
 function initMap(railsMarkers) {
           map = new google.maps.Map(document.getElementById('map'), {
@@ -37,26 +44,24 @@ function initMap(railsMarkers) {
       // };
 
 
-      // var assets = gon.assets;
-      // for (var i = 0; i < assets.length; i++) {
-      //   console.log(assets[i]["title"]);
-      // }
+
+      // console.log(assets);
+
 
       for (var i = 0; i < railsMarkers.length; i++) {
         var marker = new google.maps.Marker({
           position: new google.maps.LatLng(railsMarkers[i]["lat"], railsMarkers[i]["lng"]),
-          map: map
+          map: map,
+          icon: image,
+          title: (assets[i].price/1000).toString()
         });
         markersArray.push(marker);
         bounds.extend(marker.position);
 
 
-        // var content ='<div class="map_box" style="padding: 10px;">'+'<h3>'assets[i].title'</h3>'+
-        // '<p>'assets[i].address_combined'</p>'+'</div>';
-
         google.maps.event.addListener(marker, 'click', (function(marker, i) {
         return function() {
-            infoWindow.setContent(content);
+            infoWindow.setContent('<div><p>'+assets[i].title+'</p><p>'+'Price: '+assets[i].price+'<br>'+assets[i].address+' '+assets[i].city+' '+assets[i].post_code+' '+assets[i].country+'</p></div>');
             infoWindow.open(map, marker);
           }
         })(marker, i));
@@ -64,7 +69,6 @@ function initMap(railsMarkers) {
       };
 
       map.fitBounds(bounds);
-      console.log(gon.assets);
 
 };
 
@@ -74,21 +78,42 @@ function removeMarkers() {
     markersArray[i].setMap(null);
   }
   markersArray.length = 0;
-  console.log('Should be empty');
-  console.log(markersArray);
+  // console.log('Should be empty');
+  // console.log(markersArray);
 };
 
 
 
 function addMarkers(railsMarkers) {
+
+  var bounds_ajax = new google.maps.LatLngBounds();
+  var infoWindow = new google.maps.InfoWindow({map: map});
+
       for (var i = 0; i < railsMarkers.length; i++) {
         var marker = new google.maps.Marker({
           position: new google.maps.LatLng(railsMarkers[i]["lat"], railsMarkers[i]["lng"]),
-          map: map
+          map: map,
+          icon: image,
+          title: (assets[i].price/1000).toString()
         });
         markersArray.push(marker);
-      };
+        bounds_ajax.extend(marker.position);
 
+        google.maps.event.addListener(marker, 'click', (function(marker, i) {
+        return function() {
+            infoWindow.setContent('<div><p>'+assets[i].title+'</p><p>'+'Price: '+assets[i].price+'<br>'+assets[i].address+' '+assets[i].city+' '+assets[i].post_code+' '+assets[i].country+'</p></div>');
+            infoWindow.open(map, marker);
+          }
+        })(marker, i));
+      };
+        if (markersArray.length == 1) {
+          console.log((markersArray.length));
+          map.fitBounds(bounds_ajax);
+          map.setZoom(14);
+          map.setCenter(markerArray[i].position);
+        } else {
+        map.fitBounds(bounds_ajax);
+      }
 };
 
 function getMarkers() {
@@ -104,10 +129,24 @@ function getMarkers() {
         });
 
 
-        console.log(dynamicMarkers);
+        // console.log(dynamicMarkers);
         return dynamicMarkers;
     };
 
+function getOneMarker() {
+  var oneMarker = [];
+     $('*[data-id]').each(function(index) {
+        var coordinate_pair = {};
+        coordinate_pair["lat"]=($(this).data('latitude'));
+        coordinate_pair["lng"]=($(this).data('longitude'));
+        console.log(coordinate_pair);
+        oneMarker.push(coordinate_pair);
+        });
+
+
+        // console.log(oneMarker);
+        return oneMarker;
+    };
 
 
 
