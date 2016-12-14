28969435d9f73370fbfbdb912684cd30f7051614
Thomas Cools <tacools10@gmail.com>
2016-12-14 18:46:18 +0100

    Attempted mailer changes heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,8 +43,25 @@ Rails.application.configure do
   # config.force_ssl = true
 
   #mailer
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: "yourapp.herokuapp.com" }
+
+
+
+  config.action_mailer.default_url_options = { :host => 'findaroof.herokuapp.com' }
+
+
+  ActionMailer::Base.raise_delivery_errors = false
+  ActionMailer::Base.perform_caching = false
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: 'gmail.com',
+  user_name: ENV['GMAIL_ADDRESS'],
+  password: ENV['GMAIL_APP_PASSWORD'],
+  authentication: :login,
+  enable_starttls_auto: true
+}
+
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
@@ -59,7 +76,7 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "rails-airbnb-clone_#{Rails.env}"
-  config.action_mailer.perform_caching = false
+
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
