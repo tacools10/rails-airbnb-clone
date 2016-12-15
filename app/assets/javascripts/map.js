97d0c74f5b9f85a2bd6cb76c019ecfd1f5c7119e
Thomas Cools <tacools10@gmail.com>
2016-12-15 09:40:28 +0100

    Changed google maps style and added delete link to favorites card.

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -13,11 +13,203 @@ var asset = gon.asset;
 
 
 function initMap(railsMarkers) {
-          map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: 0, lng: 0},
-          zoom: 6
-        });
 
+      var styledMapType = new google.maps.StyledMapType(
+          [
+            {
+                "featureType": "water",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "hue": "#7fc8ed"
+                    },
+                    {
+                        "saturation": 55
+                    },
+                    {
+                        "lightness": -6
+                    },
+                    {
+                        "visibility": "on"
+                    }
+                ]
+            },
+            {
+                "featureType": "water",
+                "elementType": "labels",
+                "stylers": [
+                    {
+                        "hue": "#7fc8ed"
+                    },
+                    {
+                        "saturation": 55
+                    },
+                    {
+                        "lightness": -6
+                    },
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi.park",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "hue": "#83cead"
+                    },
+                    {
+                        "saturation": 1
+                    },
+                    {
+                        "lightness": -15
+                    },
+                    {
+                        "visibility": "on"
+                    }
+                ]
+            },
+            {
+                "featureType": "landscape",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "hue": "#f3f4f4"
+                    },
+                    {
+                        "saturation": -84
+                    },
+                    {
+                        "lightness": 59
+                    },
+                    {
+                        "visibility": "on"
+                    }
+                ]
+            },
+            {
+                "featureType": "landscape",
+                "elementType": "labels",
+                "stylers": [
+                    {
+                        "hue": "#ffffff"
+                    },
+                    {
+                        "saturation": -100
+                    },
+                    {
+                        "lightness": 100
+                    },
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "road",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "hue": "#ffffff"
+                    },
+                    {
+                        "saturation": -100
+                    },
+                    {
+                        "lightness": 100
+                    },
+                    {
+                        "visibility": "on"
+                    }
+                ]
+            },
+            {
+                "featureType": "road",
+                "elementType": "labels",
+                "stylers": [
+                    {
+                        "hue": "#bbbbbb"
+                    },
+                    {
+                        "saturation": -100
+                    },
+                    {
+                        "lightness": 26
+                    },
+                    {
+                        "visibility": "on"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.arterial",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "hue": "#ffcc00"
+                    },
+                    {
+                        "saturation": 100
+                    },
+                    {
+                        "lightness": -35
+                    },
+                    {
+                        "visibility": "simplified"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.highway",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "hue": "#ffcc00"
+                    },
+                    {
+                        "saturation": 100
+                    },
+                    {
+                        "lightness": -22
+                    },
+                    {
+                        "visibility": "on"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi.school",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "hue": "#d7e4e4"
+                    },
+                    {
+                        "saturation": -60
+                    },
+                    {
+                        "lightness": 23
+                    },
+                    {
+                        "visibility": "on"
+                    }
+                ]
+            }
+        ],
+        {name: 'Styled Map'});
+
+        map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 0, lng: 0},
+        zoom: 6,
+        mapTypeControlOptions: {
+          mapTypeIds: ['roadmap', 'satellite', 'hybrid', 'terrain',
+                    'styled_map']
+        }
+      });
+
+        map.mapTypes.set('styled_map', styledMapType);
+        map.setMapTypeId('styled_map');
 
       for (var i = 0; i < railsMarkers.length; i++) {
         var marker = new google.maps.Marker({
@@ -113,10 +305,203 @@ function getOneMarker() {
     };
 
 function initMapIndividual(railsMarkers) {
+
+  var styledMapType = new google.maps.StyledMapType(
+          [
+            {
+                "featureType": "water",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "hue": "#7fc8ed"
+                    },
+                    {
+                        "saturation": 55
+                    },
+                    {
+                        "lightness": -6
+                    },
+                    {
+                        "visibility": "on"
+                    }
+                ]
+            },
+            {
+                "featureType": "water",
+                "elementType": "labels",
+                "stylers": [
+                    {
+                        "hue": "#7fc8ed"
+                    },
+                    {
+                        "saturation": 55
+                    },
+                    {
+                        "lightness": -6
+                    },
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi.park",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "hue": "#83cead"
+                    },
+                    {
+                        "saturation": 1
+                    },
+                    {
+                        "lightness": -15
+                    },
+                    {
+                        "visibility": "on"
+                    }
+                ]
+            },
+            {
+                "featureType": "landscape",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "hue": "#f3f4f4"
+                    },
+                    {
+                        "saturation": -84
+                    },
+                    {
+                        "lightness": 59
+                    },
+                    {
+                        "visibility": "on"
+                    }
+                ]
+            },
+            {
+                "featureType": "landscape",
+                "elementType": "labels",
+                "stylers": [
+                    {
+                        "hue": "#ffffff"
+                    },
+                    {
+                        "saturation": -100
+                    },
+                    {
+                        "lightness": 100
+                    },
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "road",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "hue": "#ffffff"
+                    },
+                    {
+                        "saturation": -100
+                    },
+                    {
+                        "lightness": 100
+                    },
+                    {
+                        "visibility": "on"
+                    }
+                ]
+            },
+            {
+                "featureType": "road",
+                "elementType": "labels",
+                "stylers": [
+                    {
+                        "hue": "#bbbbbb"
+                    },
+                    {
+                        "saturation": -100
+                    },
+                    {
+                        "lightness": 26
+                    },
+                    {
+                        "visibility": "on"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.arterial",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "hue": "#ffcc00"
+                    },
+                    {
+                        "saturation": 100
+                    },
+                    {
+                        "lightness": -35
+                    },
+                    {
+                        "visibility": "simplified"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.highway",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "hue": "#ffcc00"
+                    },
+                    {
+                        "saturation": 100
+                    },
+                    {
+                        "lightness": -22
+                    },
+                    {
+                        "visibility": "on"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi.school",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "hue": "#d7e4e4"
+                    },
+                    {
+                        "saturation": -60
+                    },
+                    {
+                        "lightness": 23
+                    },
+                    {
+                        "visibility": "on"
+                    }
+                ]
+            }
+        ],
+        {name: 'Styled Map'});
+
  map = new google.maps.Map(document.getElementById('individual-map'), {
           center: {lat: 0, lng: 0},
-          zoom: 14
+          zoom: 14,
+          mapTypeControlOptions: {
+          mapTypeIds: ['roadmap', 'satellite', 'hybrid', 'terrain',
+                    'styled_map']
+        }
         });
+
+      map.mapTypes.set('styled_map', styledMapType);
+        map.setMapTypeId('styled_map');
 
 
       for (var i = 0; i < railsMarkers.length; i++) {
